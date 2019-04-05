231aa3948ebe6a9bb88537cc71785949f591f0d9
Andreas McDermott <amcd@amazon.com>
2019-04-04 22:54:02 -0700

    Fixed a few bugs.

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,27 @@ import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
-module.exports = {
+const plugins = [
+  resolve({
+    modulesOnly: true,
+  }),
+  babel({
+    exclude: 'node_modules/**'
+  })
+];
+
+module.exports = [{
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
     format: 'cjs'
   },
-  plugins: [
-    resolve({
-      modulesOnly: true,
-    }),
-    babel({
-      exclude: 'node_modules/**'
-    })
-  ]
-};
+  plugins,
+}, {
+  input: 'src/register.js',
+  output: {
+    file: 'dist/register.js',
+    format: 'cjs'
+  },
+  plugins,
+}];
